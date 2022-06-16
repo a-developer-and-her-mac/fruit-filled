a83afbb63f193a5c0be7f6e739dc5809eaafc94f
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2022-06-16 08:01:23 -0700

    feat: add array of objects to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-alert('hello world');
+const fruitList = [{
+    name: 'apple',
+    id: 1,
+    family: ''
+  },
+  {
+    name: 'pear',
+    id: 2,
+    family: ''
+  },
+  {
+    name: 'banana',
+    id: 3,
+    family: ''
+  }
+];
