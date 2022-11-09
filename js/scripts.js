072ce3570438132f378a5d6412a0185b730f96a2
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2022-11-08 19:46:15 -0800

    feat: update files and add js to eventually fetch from api

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,75 @@
-const fruitList = [{
-    name: 'apple',
-    id: 1,
-    family: ''
-  },
-  {
-    name: 'pear',
-    id: 2,
-    family: ''
-  },
-  {
-    name: 'banana',
-    id: 3,
-    family: ''
+// An IIFE
+let fruitRepository = (function () {
+  let fruitList = [];
+  let apiUrl = 'https://fruityvice.com/api/fruit/all';
+
+  //Gets all the fruits from the array
+  function getAll() {
+    return fruitList;
   }
-];
+
+  // Adds fruits to the array
+  function add(fruit) {
+    fruitList.push(fruit);
+  }
+
+  //Adds list items on the screen
+  function addListItem(fruit) {
+    let fruitRecord = document.querySelector('.fruit-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = fruit.name;
+    button.classList.add('button');
+    listItem.appendChild(button);
+    fruitRecord.appendChild(listItem);
+    button.addEventListener('click', function (event) {
+      showDetails(fruit);
+    });
+  }
+
+  //Shows more details about the selected fruit
+  function showDetails(fruit) {
+    console.log(fruit);
+  }
+
+  function loadList() {
+    return fetch(apiUrl).then((response) => {
+      return response.json();
+    }).then((json) => {
+      json.results.forEach((item) => {
+        let fruit = {
+          name: item.name,
+          nutritionsUrl: item.nutritions
+        };
+      }).catch((error) => {
+        console.log(error);
+      })
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.nutritions;
+    return fetch(url).then((reponse) => {
+      return reponse.json();
+    }).then((details) => {
+      item.sugar = details.sugar;
+      item.calories = details.calories;
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails
+  }
+})();
+//Grabs the stuff from the fruit IIFE and runs the list item function
+fruitRepository.loadList().then(() => {
+  fruitRepository.getAll().forEach((fruit) => {
+    fruitRepository.addListItem(fruit);
+  })
+})
