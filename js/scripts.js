358561a25bebdfa736ff7eec420b2f53d6ec048a
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2022-11-29 19:46:16 -0800

    Feature: add modal and dialog

--- js/scripts.js
+++ js/scripts.js
@@ -56,6 +56,99 @@ let fruitRepository = (function () {
     item.calories = url.calories;
   }
 
+  function showModal(title, text) {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.innerHTML = '';
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+
+    modalContainer.addEventListener('click', (e) => {
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+  }
+
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Modal title', 'This is the modal content!');
+  });
+
+  var dialogPromiseReject;
+
+  function hideModal() {
+    var modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+
+    if (dialogPromiseReject) {
+      dialogPromiseReject();
+      dialogPromiseReject = null;
+    }
+  }
+
+  function showDialog(title, text) {
+    showModal(title, text);
+    let modal = modalContainer.querySelector('.modal');
+    let confirmButton = document.createElement('button');
+    confirmButton.classList.add('modal-confirm');
+    confirmButton.innerText = 'Confirm';
+
+    let cancelButton = document.createElement('button');
+    cancelButton.classList.add('modal-cancel');
+    cancelButton.innerText = 'Cancel';
+
+    modal.appendChild(confirmButton);
+    modal.appendChild(cancelButton);
+
+    confirmButton.focus();
+
+    return new Promise((resolve, reject) => {
+      cancelButton.addEventListener('click', () => {
+        hideModal();
+        reject();
+      });
+      confirmButton.addEventListener('click', () => {
+        dialogPromiseReject = null;
+        hideModal();
+        resolve();
+      })
+    });
+    dialogPromiseReject = reject;
+  }
+
+  window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+
+    document.querySelector('#show-dialog').addEventListener('click', () => {
+      showDialog('Confirm action', 'Are you sure you want to do this?').then(() => {
+        alert('confirmed!');
+      }, () => {
+        alert('not confirmed');
+      })
+    });
+  });
+
   return {
     add: add,
     getAll: getAll,
